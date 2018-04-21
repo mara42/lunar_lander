da4d174dde360ab01c87f8a47acb8a801e89f344
Martti <martti@aukia.com>
2018-04-22 00:03:26 +0100

    added feature list to top of code, changed background, initial random x&y velocity, thrusting sprite, TODOs, screen-size change, rotate function made more generic, orientation changes in 1 degree increments,
    NEXT UP: instrument text rendering

--- mars_lander.py
+++ mars_lander.py
@@ -1,8 +1,33 @@
-# TODO: import all collideable objects images as pygame.sprites
-# TODO: create main loop which gets user input and updates objects respectively
-#   above refers to event.get()
-#   pygame.event.Event object contains all input
-# TODO: implement skeleton using http://pygametutorials.wikidot.com/tutorials-basic
+"""
+ORDER of implementation:
+D:
+☑ Rocket sprite
+☐ Instruments are rendered in top right corner
+☐ Behaviour for hitting screen edges (bounce, carry-over, crash)
+☐ Crash behaviour (pause, reset instruments [not score and time], reset x&y pos)
+☐ Game over behaviour (3 crashes, go out of run_game loop to game over func)
+
+C:
+☐ 3 landing pads
+☐ Crashing behaviour for pads (orientation or speed wrong)
+☐ Landing behaviour (50 points for landing, same resets as crash)
+☐ Random control failure (left or right doesn't work for 2sec, alert message)
+
+B:
+☐ 5 fixed location obstacles (10% damage for hitting)
+☐ 100% damage == disable all controls
+
+A:
+☐ random meteor storm (5-10 moving sprites, collision causes 25% damage)
+☐ Meteors disappear when hitting bottom, left or right side of screen
+
+A1:
+- Comments
+- Layout refactoring
+- Check for potential OOP mistakes or potential improvements
+- Design pattern implementaiton potential?
+"""
+import operator
 
 import pygame
 import sys
@@ -10,6 +35,7 @@ from pygame.locals import *
 import time
 from typing import List, Tuple, Dict
 from math import sin, cos, radians, ceil, floor
+import random
 
 # Some constants
 
@@ -20,13 +46,15 @@ RIGHT = 'right'
 class Game:
 
     def __init__(self):
+        pygame.init()
         self.weight: int = 1200
         self.height: int = 750
         self.size: Tuple[int, int] = (self.weight, self.height)
-        pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._display_surf = pygame.display.set_mode(
+            self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self.lander = Lander()
-        self.back_ground = Background('resources/mars_background.png', [0, 0])
+        self.back_ground = Background('resources/mars_background_instr.png',
+                                      [0, 0])
         self.clock = pygame.time.Clock()
 
     @staticmethod
@@ -50,15 +78,21 @@ class Game:
         pass
 
     def run_game(self):
+        # TODO: make everything before while 1 a function
         rotating_left = False
         rotating_right = False
         thrusting = False
-        current_vec = pygame.math.Vector2()
-        while 1:
+        initial_x_velocity = random.randint(-10, 10) / 10
+        initial_y_velocity = random.randint(0, 10) / 10
+        current_vec = pygame.math.Vector2(
+            initial_x_velocity, initial_y_velocity)
+        while True:
             Background.update_background(self.back_ground)
-
             Background.SCREEN.blit(self.lander.sprite.image,
                                    self.lander.sprite.rect)
+            if thrusting:
+                Background.SCREEN.blit(self.lander.thrust_sprite.image,
+                                       self.lander.thrust_sprite.rect)
             for event in pygame.event.get():
                 # check for closing window
                 if event.type == pygame.QUIT:
@@ -87,9 +121,14 @@ class Game:
                         thrusting = True
 
                     elif event.key == K_r:
-                        self.lander.sprite.rect.left = 300
-                        self.lander.sprite.rect.top = 150
-                        current_vec = pygame.math.Vector2()
+                        self.lander.sprite.rect.left = 600
+                        self.lander.sprite.rect.top = 0
+                        self.lander.instruments.orientation = 89
+                        self.lander.steer(RIGHT)
+                        initial_x_velocity = random.randint(-10, 10) / 10
+                        initial_y_velocity = random.randint(0, 10) / 10
+                        current_vec = pygame.math.Vector2(
+                            initial_x_velocity, initial_y_velocity)
 
             # actually do stuff based on previous flags
             if rotating_right:
@@ -98,7 +137,8 @@ class Game:
                 self.lander.steer(LEFT)
 
             if thrusting:
-                power = 0.4  # after some testing this seems good
+                power = 0.4  # after some testing this seems good, consider 0.33
+                # TODO: reduce fuel by 5 when thrusting
             else:
                 power = 0
 
@@ -106,16 +146,21 @@ class Game:
                 self.lander.instruments.orientation, power)
 
             thrust_vec = pygame.math.Vector2(new_xy)
-            # thrust_vec.scale_to_length(0.2)
-            gravity_vec = pygame.math.Vector2(0, 0.2)  # feels good
+
+            negative_thrust_vector = pygame.math.Vector2(new_xy)
+            gravity_vec = pygame.math.Vector2(0, 0.2)  # may consider 0.1
             current_vec += gravity_vec + thrust_vec
 
-            # if -1 < current_vec.length() < 1:
-            #     current_vec.y += 0.01
-
+            # TODO: add a function for handling lander hitting screen edges
             self.lander.sprite.rect.left += current_vec.x
             self.lander.sprite.rect.top += current_vec.y
-            print(current_vec)  # useful for debugging physics
+
+            if thrusting:
+                negative_thrust_vector.scale_to_length(25)
+                self.lander.rot_center(self.lander.thrust_sprite)
+            self.lander.thrust_sprite.rect.center = tuple(map(operator.sub, self.lander.sprite.rect.center, negative_thrust_vector))
+
+            #print(current_vec)  # TODO: convert to showing this in game
             self.clock.tick(60)
             pygame.display.update()
 
@@ -123,7 +168,7 @@ class Game:
 class Background(pygame.sprite.Sprite):
     # class taken from https://stackoverflow.com/a/28005796/9649969
 
-    SCREEN = pygame.display.set_mode((600, 500))
+    SCREEN = pygame.display.set_mode((1200, 750))
 
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)  # call Sprite initializer
@@ -162,15 +207,20 @@ class MovingObstacle(Obstacle):
 class Instruments:
 
     def __init__(self):
-        self.time = time.time()
-        self.fuel: int = 500
-        self.damage: int = 0
-        self.altitude: int = 1000
-        self.x_velocity: float = 0.0
-        self.y_velocity: float = 0.0
-        self.score: int = 0
+        self.time = time.time()  # min:sec, since start of 3 lives
+        self.fuel: int = 500  # kg?
+        self.damage: int = 0  # 100 == game over
+        self.altitude: int = 1000  # 0-1000m
+        self.x_velocity: float = 0.0  # m/s
+        self.y_velocity: float = 0.0  # m/s
+        self.score: int = 0  # incremented by 50
         self.lives: int = 3  # maybe move somewhere else?
-        self.orientation: int = 90  # angle, 0 & 360 mean thruster facing down
+        self.orientation: int = 90  # angle, 90 is upright, move to lander class
+        # self.sprite = Sprite('resources/instruments.png', (0, 0))
+
+    def display_instruments(self):
+        # TODO: render all instrument data
+        pass
 
     def calculate_velocity(self, angle: int, speed) -> Tuple[float, float]:
         angle = angle % 360
@@ -198,10 +248,12 @@ class Instruments:
 
 
 class Sprite(pygame.sprite.Sprite):
+    # TODO: import all collideable objects images as pygame.sprites
     # class taken from: https://stackoverflow.com/a/28005796/9649969
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image_file) #.convert()
+        self.image = pygame.image.load(image_file)  # .convert()
+        self.ORIGINALIMAGE = self.image.copy()
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
@@ -212,8 +264,8 @@ class Lander(CollidableObject):
         super().__init__("x", "y")
         self.instruments = Instruments()
         self.control_failure = None  # set to none again after 2 seconds
-        self.sprite = Sprite('resources/lander.png', (300, 150))
-        self.ORIGINALIMAGE = pygame.image.load('resources/lander.png') #.convert()
+        self.sprite = Sprite('resources/lander.png', (600, 0))
+        self.thrust_sprite = Sprite('resources/thrust.png', (600, 0))
 
     # def rot_center(self, angle):
     #     """rotate an image while keeping its center and size"""
@@ -224,13 +276,14 @@ class Lander(CollidableObject):
     #     rot_image = rot_image.subsurface(rot_rect).copy()
     #     return rot_image
 
-    def rot_center(self):
+    def rot_center(self, sprite):
         """rotate an image while keeping its center
         taken from: http://www.pygame.org/wiki/RotateCenter?parent=CookBook"""
-        rot_image = pygame.transform.rotate(self.ORIGINALIMAGE, self.instruments.orientation - 90)
-        rot_rect = rot_image.get_rect(center=self.sprite.rect.center)
-        self.sprite.image = rot_image
-        self.sprite.rect = rot_rect
+        rot_image = pygame.transform.rotate(
+            sprite.ORIGINALIMAGE, self.instruments.orientation - 90)
+        rot_rect = rot_image.get_rect(center=sprite.rect.center)
+        sprite.image = rot_image
+        sprite.rect = rot_rect
 
     def thrust(self):
         pass
@@ -239,28 +292,32 @@ class Lander(CollidableObject):
         # implement using orient %= 360 instead
         # takes 2 seconds to spin 360 degrees
         if direction == RIGHT:
-            self.instruments.orientation += 3
+            self.instruments.orientation += 1
         elif direction == LEFT:
-            self.instruments.orientation -= 3
+            self.instruments.orientation -= 1
         self.instruments.orientation %= 360  # to make sure it stays in bounds
-        self.rot_center()
+        self.rot_center(self.sprite)
 
     def crash(self):
+        # TODO: lander bottom being 1000 == crash
+        # TODO: decrement lives on crash, game over after 3
+        # TODO: implement lose screen
         pass
 
     def land(self):
+        # TODO: bot hitting pad, x & y velocity <= 5, orientation 90±5 == landing
+        # TODO: give points, pause game after landing
         pass
 
-#    def check_orientation(self):
-#        pass
-
     def float_in_direction(self, thrust: int):
+        # TODO: delete float_in_direction?
         # to deal with moving in one direction and thrust being sent to another
         # http://www.physicsclassroom.com/class/vectors/Lesson-1/Vector-Addition
         zero_velocity_position = self._calculate_new_position(thrust)
         return zero_velocity_position
 
     def _calculate_new_position(self, dist: int):
+        # TODO: delete _calculate_new_position?
         # polar coordinates
         x = dist * cos(radians(self.instruments.orientation))
         y = dist * sin(radians(self.instruments.orientation))
@@ -268,9 +325,11 @@ class Lander(CollidableObject):
         return new_position
 
     def hit_ceiling(self):
+        # TODO: prevent lander top going above 0, maybe bump with y_vel > 5±2?
         pass
 
     def pass_over_side(self):
+        # TODO: lander position left to right and vice versa when hitting edges
         pass
 
     def generate_control_failure(self):
--- vector_testing.py
+++ vector_testing.py
@@ -1,78 +1,151 @@
 from typing import Tuple
 from math import cos, sin, radians
+from PIL import Image, ImageOps
 
 import pygame
 
-x_velocity: int = 0
-y_velocity: int = 0
 
-current_position: Tuple(int, int) = (0, 0)
-new_position: Tuple(int, int) = (0, 0)
+def vector_stuff():
 
-orientation: int = 0  # max 360
+    x_velocity: int = 0
+    y_velocity: int = 0
 
-vector_without_thrust: Tuple(float, float) = (0., 0.)
-vector_with_thrust: Tuple(float, float) = (0., 0.)
-gravity_vector: Tuple(float, float) = (0., 1.0)
+    current_position: Tuple(int, int) = (0, 0)
+    new_position: Tuple(int, int) = (0, 0)
 
-final_vector = vector_with_thrust + vector_without_thrust + gravity_vector
+    orientation: int = 0  # max 360
 
-a = pygame.math.Vector2()
+    vector_without_thrust: Tuple(float, float) = (0., 0.)
+    vector_with_thrust: Tuple(float, float) = (0., 0.)
+    gravity_vector: Tuple(float, float) = (0., 1.0)
 
-distance = 1
-time = 1
+    final_vector = vector_with_thrust + vector_without_thrust + gravity_vector
 
-# speed = distance / time
+    a = pygame.math.Vector2()
 
-def calcluate_speed(gravity, current, altered):
-    pass
+    distance = 1
+    time = 1
 
+    # speed = distance / time
 
-def calculate_velocity(angle: int, speed: int = 1):
-    x_vel = speed * cos(radians(angle))
-    y_vel = speed * sin(radians(angle))
-    return x_vel, y_vel
+    def calcluate_speed(gravity, current, altered):
+        pass
 
 
-def calculate_velocity2(angle: int, speed: float = 1.0):
-    angle = angle % 360
-    if angle == 0:
-        x_vel, y_vel = speed * 1, 0
-    elif angle == 90:
-        x_vel, y_vel = 0, speed * 1
-    elif angle == 180:
-        x_vel, y_vel = speed * -1, 0
-    elif angle == 270:
-        x_vel, y_vel = 0, speed * -1
-    else:
+    def calculate_velocity(angle: int, speed: int = 1):
         x_vel = speed * cos(radians(angle))
         y_vel = speed * sin(radians(angle))
-    return x_vel, y_vel
+        return x_vel, y_vel
 
 
-def find_final_vector(vec1, vec2, vec3):
-    return vec1 + vec2 + vec3
+    def calculate_velocity2(angle: int, speed: float = 1.0):
+        angle = angle % 360
+        if angle == 0:
+            x_vel, y_vel = speed * 1, 0
+        elif angle == 90:
+            x_vel, y_vel = 0, speed * 1
+        elif angle == 180:
+            x_vel, y_vel = speed * -1, 0
+        elif angle == 270:
+            x_vel, y_vel = 0, speed * -1
+        else:
+            x_vel = speed * cos(radians(angle))
+            y_vel = speed * sin(radians(angle))
+        return x_vel, y_vel
+
+    def find_final_vector(vec1, vec2, vec3):
+        return vec1 + vec2 + vec3
+
+    grav_vec = pygame.math.Vector2(0, -0.02)
+    current_vector = pygame.math.Vector2()
+    user_vector = pygame.math.Vector2(0, 1)  # important that length is 0.12
+
+    dist = 0
+
+    for _ in range(int(60*2.5)):
+        user_vector.scale_to_length(0.12)
+        current_vector = find_final_vector(current_vector, grav_vec,
+                                           user_vector)
+        print(current_vector.y)
+        dist += current_vector.y
+
+    print(current_vector, 'd:', dist)
+
+    ##############
+
+    # velocity_vector = (speed, direction)
 
 
-grav_vec = pygame.math.Vector2(0, -0.02)
-current_vector = pygame.math.Vector2()
-user_vector = pygame.math.Vector2(0, 1)  # important that length is 0.12
+def pillow_stuff():
+    desired_size = 368
+    im_pth = "resources/lander2.png"
+
+    im = Image.open(im_pth)
+    old_size = im.size  # old_size[0] is in (width, height) format
+
+    ratio = float(desired_size)/max(old_size)
+    new_size = tuple([int(x*ratio) for x in old_size])
+    # use thumbnail() or resize() method to resize the input image
+
+    # thumbnail is a in-place operation
+
+    # im.thumbnail(new_size, Image.ANTIALIAS)
+
+    # im = im.resize(new_size, Image.ANTIALIAS)
+    # create a new image and paste the resized on it
+
+    delta_w = desired_size - new_size[0]
+    delta_h = desired_size - new_size[1]
+    padding = (delta_w // 2, delta_h // 2, delta_w - (delta_w // 2),
+               delta_h - (delta_h // 2))
+    new_im = ImageOps.expand(im, padding)
+
+    new_im.show()
 
 
-dist = 0
+def cv2_method():
+    import cv2
 
-for _ in range(int(60*2.5)):
-    user_vector.scale_to_length(0.12)
-    current_vector = find_final_vector(current_vector, grav_vec,
-                                       user_vector)
-    print(current_vector.y)
-    dist += current_vector.y
+    desired_size = 368
+    im_pth = "resources/lander2.png"
 
-print(current_vector, 'd:', dist)
+    im = cv2.imread(im_pth)
+    old_size = im.shape[:2]  # old_size is in (height, width) format
 
-##############
+    ratio = float(desired_size) / max(old_size)
+    new_size = tuple([int(x * ratio) for x in old_size])
 
-# velocity_vector = (speed, direction)
+    # new_size should be in (width, height) format
+
+    im = cv2.resize(im, (new_size[1], new_size[0]))
+
+    delta_w = desired_size - new_size[1]
+    delta_h = desired_size - new_size[0]
+    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+    left, right = delta_w // 2, delta_w - (delta_w // 2)
+
+    color = [0, 0, 0]
+    new_im = cv2.copyMakeBorder(im, top, bottom, left, right,
+                                cv2.BORDER_CONSTANT,
+                                value=color)
+
+    cv2.imshow("image", new_im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
+from PIL import Image
 
+
+def make_square(im, min_size=77, fill_color=(0, 0, 0, 0)):
+    x, y = im.size
+    size = max(min_size, x, y)
+    new_im = Image.new('RGBA', (size, size), fill_color)
+    new_im.paste(im, ((size - x) // 2, (size - y) // 2))
+    return new_im
+
+
+if __name__ == '__main__':
+    test_image = Image.open('resources/lander2.png')
+    new_image = make_square(test_image)
+    new_image.show()
